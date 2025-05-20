7802d383c9e5e7b1680cc0bd3638a5176427fa3b
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2025-05-20 10:30:39 +0800

    feat: remove structure optimization for NEB

--- lambench/tasks/calculator/neb/neb.py
+++ lambench/tasks/calculator/neb/neb.py
@@ -10,17 +10,15 @@ from sklearn.metrics import mean_absolute_error
 def run_inference(
     model: ASEModel,
     test_data: Path,
-    fmax: float,
-    steps: int,
 ) -> dict[str, float]:
     """ "
     This tests perform energy barrier prediction for 460 OOD NEB trajs in the OC20NEB dataset.
 
-    The label.csv file contains the ground truth energy barrier and dE for each trajectory.
+    The label.csv file contains the ground truth energy barrier Ea and dE for each trajectory.
     Each trajectory file contains 10 frames, and the first and last frames are the initial and final states.
     Using the ground truth energy label, the index of the transition state is determined.
 
-    We then perform structure optimization for the initial, final, and transition states and calculate the energy barrier and dE.
+    We then perform single point inference for the initial, final, and transition states and calculate Ea and dE.
     """
     result_df = pd.read_csv(Path(test_data, "label.csv"))
     result_df.sort_values("traj", inplace=True)
@@ -36,20 +34,12 @@ def run_inference(
         barrier_idx = np.argmax(energies)
 
         initial, transition, final = data[0], data[barrier_idx], data[-1]
-        relaxed_energy = []
+        pred_energy = []
         for atoms in [initial, transition, final]:
-            atoms = ASEModel.run_ase_relaxation(
-                atoms=atoms,
-                calc=model.calc,
-                fmax=fmax,
-                steps=steps,
-                fix_symmetry=False,
-                relax_cell=False,
-            )
-            relaxed_energy.append(atoms.get_potential_energy())
+            pred_energy.append(atoms.get_potential_energy())
         e_a, de = (
-            relaxed_energy[1] - relaxed_energy[0],
-            relaxed_energy[-1] - relaxed_energy[0],
+            pred_energy[1] - pred_energy[0],
+            pred_energy[-1] - pred_energy[0],
         )
         result_df.at[idx, "pred_Ea"] = e_a
         result_df.at[idx, "pred_dE"] = de
