1a8310f0acfd1ed01bb5cfad0106a9eb7da5d97a
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2025-10-21 15:28:35 +0800

    chore: reformat

--- lambench/tasks/calculator/vacancy/vacancy.py
+++ lambench/tasks/calculator/vacancy/vacancy.py
@@ -13,7 +13,6 @@ import numpy as np
 from ase import Atoms
 from tqdm import tqdm
 from pathlib import Path
-from ase.io import read
 
 from sklearn.metrics import root_mean_squared_error, mean_absolute_error
 
@@ -21,10 +20,8 @@ from lambench.models.ase_models import ASEModel
 import logging
 
 
-
 def get_oxygen_reference_energy(calc) -> float:
-
-    vacuum_size = 30      # Ångströms: Large cell size to ensure vacuum separation
+    vacuum_size = 30  # Ångströms: Large cell size to ensure vacuum separation
     o_o_bond_length = 1.23  # Ångströms: Experimental O-O bond length for O2
     cell_vector = vacuum_size
     cell = [cell_vector, cell_vector, cell_vector]
@@ -32,17 +29,13 @@ def get_oxygen_reference_energy(calc) -> float:
 
     positions = [
         (center, center, center - o_o_bond_length / 2),
-        (center, center, center + o_o_bond_length / 2)
+        (center, center, center + o_o_bond_length / 2),
     ]
 
-    molecular_oxygen = Atoms(
-        'O2',
-        positions=positions,
-        cell=cell,
-        pbc=True
-    )
+    molecular_oxygen = Atoms("O2", positions=positions, cell=cell, pbc=True)
     molecular_oxygen.calc = calc
-    return molecular_oxygen.get_potential_energy()/2
+    return molecular_oxygen.get_potential_energy() / 2
+
 
 def run_inference(
     model: ASEModel,
@@ -52,7 +45,6 @@ def run_inference(
     defect_structures = read(test_data / "vacancy_defect_structures.traj", ":")
     labels = np.load(test_data / "vacancy_evf_label.npy")
 
-
     evf_lab = []
     evf_pred = []
     calc = model.calc
@@ -60,11 +52,12 @@ def run_inference(
     # Calculate reference energy for oxygen atom
     E_o = get_oxygen_reference_energy(calc)
 
-    for pristine, defect, label in tqdm(zip(pristine_structures, defect_structures, labels)):
-
+    for pristine, defect, label in tqdm(
+        zip(pristine_structures, defect_structures, labels)
+    ):
         natoms_pri = len(pristine)
         natoms_def = len(defect)
-        
+
         n_oxygen = natoms_pri - natoms_def
 
         pristine.calc = calc
@@ -79,7 +72,7 @@ def run_inference(
 
         except Exception as e:
             logging.error(f"Error occurred while processing structures: {e}")
-        
+
     return {
         "MAE": mean_absolute_error(evf_lab, evf_pred),  # eV
         "RMSE": root_mean_squared_error(evf_lab, evf_pred),  # eV
