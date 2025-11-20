6355e8bb964258d868822882b1fcf8c988aa7d91
Anyang Peng <137014849+anyangml@users.noreply.github.com>
2025-11-20 17:29:07 +0800

    debug

--- lambench/workflow/dflow.py
+++ lambench/workflow/dflow.py
@@ -56,7 +56,7 @@ def submit_tasks_dflow(
         name = "".join([c if c.isalnum() else "-" for c in name])
         if task.test_data is not None:
             # handle dict type test_data, NOTE: if the datasets are in the same parent folder, only need to upload the artifact once.
-            task_data = list(task.test_data.values()[0]) if isinstance(task.test_data, dict) else [task.test_data]
+            task_data = list(task.test_data.values())[0] if isinstance(task.test_data, dict) else task.test_data
         else:
             task_data = []
         logging.warning(f"Submitting task {name} with test data paths: {task_data}")
@@ -76,7 +76,7 @@ def submit_tasks_dflow(
                 "task": task,
                 "model": model,
             },
-            artifacts={"dataset": get_dataset([model.model_path] + task_data)},
+            artifacts={"dataset": get_dataset([model.model_path + task_data])},
             executor=DispatcherExecutor(
                 machine_dict={
                     "batch_type": "Bohrium",
